1df85220411c01ec160d500a79a885cf46412209
Akshay-Codespace <akshaymutkule942057@gmail.com>
2026-04-04 22:37:28 +0530

    Add files via upload

---
+++ python/Practice_QS/Practice_QS_Error_ExceptionHandling.py
@@ -0,0 +1,206 @@
+#PART 1: Basic Try–Except Practice
+#1 Simple Division Program
+# Write a program that:
+#
+# Takes two numbers from the user
+#
+# Divides them
+#
+# Handles:
+#
+# ZeroDivisionError
+#
+# ValueError
+try:
+   num1 = int(input("Enter a number: "))
+   num2 = int(input("Enter another number: "))
+   result = num1/num2
+except ZeroDivisionError:
+    print("Division by zero")
+except ValueError:
+    print("Invalid input")
+
+#2
+# Convert Input to Integer
+#
+# Write a program that:
+#
+# Takes user input
+#
+# Converts it to an integer
+#
+# If the user enters text, print:
+# "Please enter a valid number"
+
+try:
+    num3 = input("Enter a number: ")
+    num4 = int(num3)
+    print(num4)
+except ValueError:
+    print("Invalid input please enter a number")
+
+#3 Access List Element
+
+# Create a list:
+# numbers = [10, 20, 30, 40, 50]
+# Write a program that:
+#Asks the user to enter an index
+# Prints the element at that index
+# Handles IndexError
+try:
+    numbers = [10, 20, 30, 40, 50]
+    insert = int(input("Enter a number: "))
+    print("Your index element is :", numbers[insert])
+except IndexError:
+    print("Index out of range")
+
+#4 String + Number Error
+# Write a program that
+# Adds a number and a string
+# Handles TypeError
+# Prints a friendly message
+
+try:
+    num3 = int(input("Enter a number: "))
+    str1 = str(input("Enter a string: "))
+    print(num3+str1)
+except TypeError:
+    print("its not possible to concate str or int")
+
+#5Multiple Exceptions
+
+# Write a small calculator that:
+# Takes two numbers from the user
+# Divides them
+# Handles:
+# ValueError
+# ZeroDivisionErro
+
+try:
+    num5 = int(input("Enter a number: "))
+    num6 = int(input("Enter another number: "))
+    print(num5/num6)
+except ZeroDivisionError:
+    print("it cant Divisible by zero")
+except ValueError:
+    print("Invalid input")
+
+#  PART 2: Basic raise Practice
+
+#6  Age Validation
+
+# Write a program that:
+#
+# Takes age as input
+#
+# If age < 0 → raise ValueError("Age cannot be negative")
+
+try:
+   age = int(input("Enter a number: "))
+   if age<0:
+       raise ValueError("Your age cannot be negative")
+   print(age,"is valid")
+except ValueError:
+   print("your age is not become -ve")
+
+
+#7 Marks Validation
+# Write a program that:
+# Takes marks as input
+# If marks < 0 or marks > 100 → raise ValueError
+
+try:
+    marks = int(input("Enter a number: "))
+    if marks < 0:
+        raise ValueError("Your marks cannot be negative")
+    print(marks,"is valid")
+except Exception as e:
+    print("generic error",e)
+
+
+#8 Password Length Check
+# Write a program that:
+# Takes password from the user
+# If password length < 6 → raise an error
+
+
+try:
+    pas = int(input("Enter a number: "))
+    if pas.length < 6:
+        raise ValueError("Your passage cannot be less than 6")
+    print(pas,"is valid")
+except Exception as e:
+    print("generic error",e)
+
+#  PART 3: Using finally
+# 9  Basic Finally Example
+#
+# Write a program that:
+#
+# Takes a number
+#
+# Divides 100 by that number
+#
+# Uses finally to print:
+# "Program Finished"
+
+try:
+    num7 = int(input("Enter a number: "))
+    mock = 100/num7
+    print(mock,"is valid")
+except ZeroDivisionError:
+    print("Division by zero")
+except ValueError:
+    print("Invalid input")
+finally:
+    print("Program finishd")
+
+#10 File Example
+# Write a program that:
+# Opens a file
+# Reads its content
+# Uses finally to print:
+# "File operation -"
+
+
+# try:
+#     with open("Practice_QS_operator.py", "r") as file:
+#         content = file.read()
+#         print("File Content:")
+#         print(content)
+# except FileNotFoundError:
+#     print("File not found.")
+
+
+try:
+    file = open("Practice_QS_operator.py", "r")
+    content = file.read()
+    print(content)
+    file.close()
+except FileNotFoundError:
+    print("File not found.")
+
+
+
+
+
+
+
+
+
+
+
+#11
+# Safe Input Program
+# Write a program that:
+# Keeps asking the user for a numbe
+# Stops only when a valid number is entered
+# Uses try–except
+
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        print("You entered:", num)
+        break
+    except ValueError:
+        print("Invalid input! Please enter a valid number.")
---
+++ python/Practice_QS/Practice_QS_List.py
@@ -0,0 +1,235 @@
+# 1
+
+list1 = [2,5,7,8,6]
+print(list1)
+
+#2
+
+list2 = [2,3,4,"akshay","rushikesh",3.5]
+print(list2)
+
+#3
+list3 = []
+list3.append(3,)
+list3.append(5,)
+list3.append(7,)
+print(list3)
+
+#4 Create a list of 5 fruits and print the third fruit
+
+list4 = ["mango", "banana","apple","chiku"]
+print(list4[2])
+
+#5 Print the last element of a list using negative indexing.
+
+list5 =  [2,4,5,78,7]
+print(list5[-1])
+
+
+# level 2
+#6
+nums = [10, 20, 30, 40, 50, 60]
+
+
+# Print elements from index 1 to 4.
+
+# Print every second element.
+
+# Reverse the list using slicing.
+
+print(nums[0:5])
+print(nums[::2])
+print(nums[::-1])
+
+#7 Extract first 3 elements from a list.
+
+print(nums[0:3])
+
+#8 Extract last 2 elements using negative indexing
+
+print(nums[-2:])
+
+#9  Write a program to check whether an element exists in a list.
+
+counts = nums.count(10)
+print(counts)
+
+
+#level 3  List Methods Practice
+
+#10 Add 3 grocery items using append().
+
+grocery = []
+grocery.append("mango")
+grocery.append("banana")
+grocery.append("apple")
+
+print(grocery)
+
+#11 Add multiple items using extend().
+
+grocery.extend(["chiku","coconut","guava","chery"])
+print(grocery)
+
+#12   Insert "Oil" at index 2 in a grocery list
+
+grocery.insert(2,"oil")
+print(grocery)
+
+#13 Remove a specific item from a list.
+
+grocery.remove("chery")
+print(grocery)
+
+
+#14  Remove last element using pop().
+
+grocery.pop()
+print(grocery)
+
+#15  Clear the entire list using clear().
+
+
+grocery.clear()
+print(grocery)
+
+#16 Count how many times 5 appears in: nums = [1, 5, 3, 5, 7, 5]
+
+nums = [1, 5, 3, 5, 7, 5]
+print(nums.count(5))
+
+#17   Find index of "Apple" in a fruit list.
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits.index("apple"))
+
+#18  Sort a list of numbers.
+
+nums.sort()
+print(nums)
+
+#19 Reverse a list using reverse().
+
+nums.reverse()
+print(nums)
+
+
+#Level 4   Logic-Based Questions
+
+#20  Write a program to find the largest element in a list.
+
+list = [1,2,3,4,5,6,7,8,9]
+list.sort()
+print(list)
+print(list[-1])
+
+#21  Write a program to find the smallest element in a list.
+
+list2 = [76,45,56,78,32,77,237,]
+list2.sort()
+print(list2[0])
+
+#22 Calculate sum of all elements in a list.
+
+list3 = [34,6,78,34,78,23]
+print(sum(list3))
+
+
+#23 Count even and odd numbers in a list
+
+list4 = [43,34,5,3,56,32,34,78]
+
+
+#24 Remove duplicates from a list.
+
+list5 = [34,56,87,90,78,564,34]
+
+
+#25  Merge two lists into one.
+
+list6 = [32,56,76,8,74,84]
+list7 = [63,5,6,4,74,3]
+print(list6 + list7)
+
+
+# level 5
+#26  Create a list of 10 numbers using user input.
+
+user = []
+ii = int(input("Enter a number: "))
+user.append(ii)
+user.append(1)
+print(user)
+
+#27  Replace the second element of a list with a new value.
+
+list8 = [23,56,87,3,23,8,9]
+list8[1] = 88
+print(list8)
+
+#28 Swap first and last elements of a list.
+
+list9 = [3,657,56,467,45,74]
+list9[0], list9[-1] = list9[-1], list9[0]
+print(list9)
+
+#29 Find the second largest number in a list.
+
+list10 = [23,65,3275,3354,63433,56]
+list10.sort()
+print(list10[-2])
+
+# 30  Check if a list is a palindrome.
+
+list11 = [1,2,1]
+lst = (list11[::-1])
+print(lst)
+print(list11)
+print("it is Palindrome",list11 == lst)
+
+# Level 6
+#  Mini Practical Tasks
+
+#31  Grocery List Program ,Add items ,Remove items ,Display total number of items
+grocery12 = ["apple", "banana","orange","milk"]
+grocery12.append("chocolate")
+grocery12.append("cherry")
+grocery12.remove("chocolate")
+print(grocery12)
+
+#32 Student Marks System, Store marks in a list,  Find average, Find highest and lowest
+
+marks12 = []
+m = int(input("enter a marks:" ))
+n = int(input("enter a marks:"))
+o = int(input("enter a marks:"))
+marks12.append(m)
+marks12.append(n)
+marks12.append(o)
+
+print(marks12)
+print("Average",(m + n + o)/3)
+
+if m>n and m>o:
+    print("m is greater")
+elif n>m and n>o:
+    print("n is greater")
+else:
+    print("o is greater")
+
+
+#33Shopping Cart ,Add item prices, Calculate total bill, Remove item price if needed
+
+cart =[]
+cart.append(23)
+cart.append(34)
+cart.append(343)
+
+bill = (cart[0]+ cart[1]+ cart[2])
+
+if bill > 150:
+    cart.remove(34)
+    print(cart)
+else:
+    print("no need")
+
